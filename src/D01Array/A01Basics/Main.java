4642ac36957d5b080a4db6b0ce461ae92933ac7c
qiaogaojian <qiaogaojian@vip.qq.com>
2021-10-26 23:13:13 +0800

    feat:确定代码分类组织结构

---
+++ src/D01Array/A01Basics/Main.java
@@ -0,0 +1,14 @@
+package D01Array.A01Basics;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Hello();
+    }
+
+    static void Hello()
+    {
+        System.out.println("Hello World!");
+    }
+}
