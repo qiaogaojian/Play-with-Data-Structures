df7d5884f812dcf8b6eba2a4755be89427908af0
qiaogaojian <qiaogaojian@vip.qq.com>
2021-10-26 23:27:03 +0800

    feat: Array01 Basics

--- src/D01Array/A01Basics/Main.java
+++ src/D01Array/A01Basics/Main.java
@@ -4,11 +4,25 @@ public class Main
 {
     public static void main(String[] args)
     {
-        Hello();
-    }
+        int[] arr = new int[20];
+        for (int i = 0; i < arr.length; i++)
+        {
+            arr[i] = i;
+            System.out.print(String.format("%d ",arr[i]));
+        }
+        System.out.println("\n");
 
-    static void Hello()
-    {
-        System.out.println("Hello World!");
+        int[] scores = new int[]{100, 99, 66};
+        for (int score : scores)
+        {
+            System.out.println(score);
+        }
+        System.out.println("\n");
+
+        scores[0] = 98;
+        for (int i = 0; i < scores.length; i++)
+        {
+            System.out.println(scores[i]);
+        }
     }
 }
