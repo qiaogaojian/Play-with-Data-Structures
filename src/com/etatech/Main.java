e30c3a7afa3408f72a16d3a291b67a68fce4dc5e
qiaogaojian <qiaogaojian@vip.qq.com>
2021-10-26 22:42:12 +0800

    feat: init project

---
+++ src/com/etatech/Main.java
@@ -0,0 +1,15 @@
+package com.etatech;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        Hello();
+    }
+
+    static void Hello()
+    {
+        System.out.println("Hello World!");
+    }
+}
